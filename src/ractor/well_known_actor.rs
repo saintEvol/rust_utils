08636a1f996da8621529a2016bda31e30d6b4c85
john <casle_death@163.com>
2024-11-13 16:59:00 +0800

    优化well_know_actor的实现

--- src/ractor/well_known_actor.rs
+++ src/ractor/well_known_actor.rs
@@ -1,4 +1,7 @@
-
+#[cfg(feature = "ractor")]
+use ractor::{Actor, ActorCell, ActorRef, SpawnErr};
+#[cfg(feature = "ractor")]
+use ractor::concurrency::JoinHandle;
 
 #[cfg(feature = "ractor")]
 /// 一个被全局知晓的Actor进程，可以获取到其进程id
@@ -7,10 +10,13 @@ pub trait WellKnownActor: ractor::Actor {
     fn global_name() -> ractor::ActorName;
 
     /// 获取自己的进程id
-    fn me() -> Option<ractor::ActorRef<<Self as ractor::Actor>::Msg>> {
+    fn me() -> Option<ActorRef<<Self as ractor::Actor>::Msg>> {
         ractor::registry::where_is(<Self as WellKnownActor>::global_name()).map(|cell|cell.into())
     }
 
+    async fn spawn_linked(self, args: <Self as Actor>::Arguments, supervisor: ActorCell) -> Result<(ActorRef<Self::Msg>, JoinHandle<()>), SpawnErr> {
+        Actor::spawn_linked(Some(Self::global_name()), self, args, supervisor)
+    }
     // fn cast(msg: <Self as ractor::Actor>::Msg) {
     //     use ractor::{cast, ActorRef};
     //
