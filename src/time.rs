807c6ffdff03cbada70ccc5d6adde99d1b66b4f7
john <casle_death@163.com>
2025-01-12 16:36:13 +0800

    增加更多时间格式化函数

--- src/time.rs
+++ src/time.rs
@@ -1,4 +1,4 @@
-use chrono::{Datelike, Local, Timelike};
+use chrono::{DateTime, Datelike, Local, TimeZone, Timelike};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 /// 当前微秒数
@@ -31,27 +31,40 @@ pub fn now_secs() -> u64 {
 /// 将当前时间秒数转换为字符串
 pub fn now_secs_str<T: std::fmt::Display>(suffix: Option<T>) -> String {
     let now = Local::now();
+    format_datetime(now, suffix)
+}
+
+/// 给定一个秒数，将其格式化为字符串
+pub fn format_secs<T: std::fmt::Display>(secs: u64, suffix: Option<T>) -> String {
+    let datetime: DateTime<Local> = Local.timestamp_opt(secs as i64, 0).unwrap();
+    format_datetime(datetime, suffix)
+}
+
+pub fn format_datetime<T: std::fmt::Display>(
+    datetime: DateTime<Local>,
+    suffix: Option<T>,
+) -> String {
     if let Some(ref s) = suffix {
         let timestamp = format!(
             "{:04}{:02}{:02}{:02}{:02}{:02}{}",
-            now.year(),
-            now.month(),
-            now.day(),
-            now.hour(),
-            now.minute(),
-            now.second(),
+            datetime.year(),
+            datetime.month(),
+            datetime.day(),
+            datetime.hour(),
+            datetime.minute(),
+            datetime.second(),
             s
         );
         timestamp
     } else {
         let timestamp = format!(
             "{:04}{:02}{:02}{:02}{:02}{:02}",
-            now.year(),
-            now.month(),
-            now.day(),
-            now.hour(),
-            now.minute(),
-            now.second(),
+            datetime.year(),
+            datetime.month(),
+            datetime.day(),
+            datetime.hour(),
+            datetime.minute(),
+            datetime.second(),
         );
         timestamp
     }
