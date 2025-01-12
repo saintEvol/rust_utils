51128c92d80cc8ded478443dec2369f572861c5c
john <casle_death@163.com>
2025-01-12 16:18:47 +0800

    增加生成当前秒数的时间戳格式化字符串的函数

--- src/time.rs
+++ src/time.rs
@@ -1,3 +1,4 @@
+use chrono::{Datelike, Local, Timelike};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 /// 当前微秒数
@@ -25,4 +26,33 @@ pub fn now_secs() -> u64 {
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
     since.as_secs()
+}
+
+/// 将当前时间秒数转换为字符串
+pub fn now_secs_str<T: std::fmt::Display>(suffix: Option<T>) -> String {
+    let now = Local::now();
+    if let Some(ref s) = suffix {
+        let timestamp = format!(
+            "{:04}{:02}{:02}{:02}{:02}{:02}{}",
+            now.year(),
+            now.month(),
+            now.day(),
+            now.hour(),
+            now.minute(),
+            now.second(),
+            s
+        );
+        timestamp
+    } else {
+        let timestamp = format!(
+            "{:04}{:02}{:02}{:02}{:02}{:02}",
+            now.year(),
+            now.month(),
+            now.day(),
+            now.hour(),
+            now.minute(),
+            now.second(),
+        );
+        timestamp
+    }
 }
