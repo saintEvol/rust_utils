04c211af31c151268b122c1f282568c9bd5124e2
john <casle_death@163.com>
2025-02-15 11:26:53 +0800

    移除数据库列表扩展的泛型版本

--- src/types/database_expand.rs
+++ src/types/database_expand.rs
@@ -4,11 +4,20 @@
 use sea_orm::FromJsonQueryResult;
 use serde::{Deserialize, Serialize};
 
+// #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
+// pub struct ListType<T>(pub Vec<T>);
+
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
-pub struct ListType<T>(pub Vec<T>);
+pub struct U32List(pub Vec<u32>);
 
-// #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
-// pub struct U32List(pub Vec<u32>);
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
+pub struct I32List(pub Vec<i32>);
 
-// #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
-// pub struct StringList(pub Vec<String>);
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
+pub struct U64List(pub Vec<u64>);
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
+pub struct I64List(pub Vec<i64>);
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, FromJsonQueryResult)]
+pub struct StringList(pub Vec<String>);
