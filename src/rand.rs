91468fc12d5a5ce8e44006829bd23e469f7fb5e0
john <casle_death@163.com>
2025-02-24 17:43:26 +0800

    移动加密码函数的位置

--- src/rand.rs
+++ src/rand.rs
@@ -18,17 +18,3 @@ pub fn rand_s(length: usize) -> String {
 
     rand_string
 }
-
-/// 使用盐将密码进行加密
-#[cfg(feature = "encrypt")]
-pub fn encrypt_password(password: &str, salt: &str) -> String {
-    use std::fmt::Write;
-    let s = password.to_owned() + salt;
-    let digest = md5::compute(s).to_vec();
-
-    let mut result = String::new();
-    for a in digest.iter() {
-        write!(result, "{:02x}", a).unwrap();
-    }
-    result
-}
