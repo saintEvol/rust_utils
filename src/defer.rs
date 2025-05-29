a4e948fe53d469549456f21c3bae402ee56344d3
john <casle_death@163.com>
2025-05-29 20:53:39 +0800

    增加注释

--- src/defer.rs
+++ src/defer.rs
@@ -25,6 +25,7 @@ where
     }
 
     /// 放弃扫尾操作，直接返回数据，因为只能通过[new](Self::new)构建，并且，本接口会消费掉自己，所以可以保证一定存在数据
+    /// 应该只在完全确认必要的情况下才调用本函数放弃后续的扫尾逻辑
     pub fn give_up(mut self) -> T {
         self.defer.take();
         let data = self.data.take().unwrap();
